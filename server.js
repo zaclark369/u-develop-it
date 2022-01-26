5225271ec5a469f0ea500ba173b78aefd96a18b2
Zachary Clark <zacclark369@gmail.com>
2022-01-25 17:36:44 -0800

    added database query method

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// connect to DATABASE
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Redtail369!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //   Default response for any other request (NOT FOUND)
 app.use((req, res) => {
