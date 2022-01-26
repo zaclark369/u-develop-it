33817cd0ea7624c6999d7eaa0b69adcf24c481a3
Zachary Clark <zacclark369@gmail.com>
2022-01-25 17:45:56 -0800

    made query's and tested, will be incorperated into express.js routes

--- server.js
+++ server.js
@@ -19,9 +19,38 @@ const db = mysql.createConnection(
     console.log('Connected to the election database')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+// delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?, ?, ?, ?)`;
+
+// const params = [1, 'Ronald', 'Firbandk', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
 
 //   Default response for any other request (NOT FOUND)
 app.use((req, res) => {
