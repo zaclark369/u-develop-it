79034d812245ea4d342fba1cb422665638c7ff2b
Zachary Clark <88634696+zaclark369@users.noreply.github.com>
2022-01-27 19:31:41 -0800

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
       host: "localhost",
       user: "root",
-      password: "Redtail369!",
+      password: "password",
       database: "election",
     },
     console.log("Connected to the election database")
