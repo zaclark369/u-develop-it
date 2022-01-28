d2a2ec505959485485c75887a2d3a86cf156ff8d
Zachary Clark <zacclark369@gmail.com>
2022-01-27 17:34:30 -0800

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 
 module.exports = router;
